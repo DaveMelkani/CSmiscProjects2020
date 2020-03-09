0001f6a833949235bf2db0f396a8996c425e5516
1905093 <1905093@00-46269.pusd.dom>
2020-03-09 09:31:43 -0700

    update on kirby dab (3.9.2020)

--- KirbyTheExplorer/src/KirbyDiugh.java
+++ KirbyTheExplorer/src/KirbyDiugh.java
@@ -1,7 +1,8 @@
 import java.io.File;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class KirbyDiugh {
@@ -9,52 +10,61 @@ public class KirbyDiugh {
 		// keep input file in the parent folder for the project
 		// not in the src folder
 		String i;
-		File file = new File("map2"); // point to file
+		File file = new File("map5cord"); // point to file
 
 		try {
 
 			Scanner sc = new Scanner(file); // setup scanner
-			
+
 			String a = sc.next();
 			String b = sc.next();
 			String c = sc.next();
-			
+
 			int x = Integer.parseInt(a);
 			System.out.print(x);
-			
+
 			int y = Integer.parseInt(b);
-			System.out.print(" "+y);
-			
+			System.out.print(" " + y);
+
 			int r = Integer.parseInt(c);
-			System.out.println(" "+r);
-			
+			System.out.println(" " + r);
+
 			String[][][] arr = new String[x][y][r];
 			String test = sc.nextLine();
 			System.out.println(test);
-			
-//			try{
-//				if(!(test.length() == 0)) duck = Integer.parseInt(test.substring(test.length()-1));
-//				int davedab = Integer.parseInt(test.substring(test.length()-1));
-//				int footdab = Integer.parseInt(test.substring(test.length()-3, test.length()-2));
-//				String strdab = test.substring
-//			}
-			
-//				int cntr = 0;
-				// check a line exists in the input file
+
+			// try{
+			// if(!(test.length() == 0)) duck =
+			// Integer.parseInt(test.substring(test.length()-1));
+			// int davedab = Integer.parseInt(test.substring(test.length()-1));
+			// int footdab = Integer.parseInt(test.substring(test.length()-3,
+			// test.length()-2));
+			// String strdab = test.substring
+			// }
+
+			// int cntr = 0;
+			// check a line exists in the input file
+			try {
 				while (sc.hasNextLine()) {
 					// NOTE format of input file is expected
 					for (int k = 0; k < r; k++) {
-					for (int cntr = 0; cntr < x; cntr++) {
-//						i = sc.next();
-						String val = sc.next();
-						for (int d = 0; d < y; d++) {
-//							System.out.println(i);
-							String temp = val.substring(d, d+1);
-							arr[cntr][d][k] = temp;
+						for (int cntr = 0; cntr < x; cntr++) {
+							// i = sc.next();
+							String val = sc.next();
+							for (int d = 0; d < y; d++) {
+								// System.out.println(i);
+								String temp = val.substring(d, d + 1);
+								arr[cntr][d][k] = temp;
+							}
 						}
+						// call nextInt()
 					}
-					// call nextInt()
 				}
+			} catch (NoSuchElementException e) {
+				System.out.println("No Such Element Exception");
+			} catch (StringIndexOutOfBoundsException n) {
+				System.out.println("StringIndexOutOfBoundsException");
+				n.printStackTrace();
 			}
 			sc.close(); // done with scanner
 			for (int ew = 0; ew < arr[0][0].length; ew++) {
